669551d8ecba9769547352e9220d9861358c9d6d
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-14 18:17:32 -0500

    Created initial file structure and products table

---
+++ bamazonCustomer.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: process.env.SQL_PASS,
+  database: "bamazon_DB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
---
+++ bamazonDB.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(45) NULL,
+  price INT default 1,
+  stock_quantity INT default 1,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("chair", "furniture", 150, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("coffee mug", "home goods", 5, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("slippers", "clothing", 50, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("phone charger", "electronics", 10, 80);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("candle", "home goods", 12, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("desk", "furniture", 400, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("sweater", "clothing", 40, 120);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("monitor", "electronics", 200, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("calendar", "home goods", 15, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("rug", "furniture", 350, 12);
