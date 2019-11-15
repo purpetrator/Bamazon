0b6dc840388a927c72f5ca9101301011a29a6bb8
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-14 21:03:07 -0500

    Created inquirer prompts for user to select items and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,6 +17,7 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   displayAllItems();
+  buyProduct();
   connection.end();
 });
 
@@ -31,9 +33,77 @@ function displayAllItems() {
           " - " +
           res[i].department_name +
           " - " +
-          res[i].price
+          "$" +
+          res[i].price +
+          " - " +
+          "qty: " +
+          res[i].stock_quantity
       );
     }
     console.log("-----------------------------------");
+  });
+}
+
+function buyProduct() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // console.log(res);
+
+    inquirer
+      .prompt([
+        {
+          name: "whichProduct",
+          type: "rawlist",
+          message:
+            "\nWhich product would you like to buy? Please choose a product by its ID.",
+          choices: function() {
+            var productsArray = [];
+            for (var i = 0; i < res.length; i++) {
+              productsArray.push(res[i].item_id);
+            }
+            return productsArray;
+          }
+        },
+        {
+          type: "input",
+          name: "qtyProduct",
+          message: "\nPlease enter the quantity you would like to buy: "
+        }
+      ])
+      .then(function(answer) {
+        // console.log(answer);
+        var chosenItem;
+        for (var i = 1; i < res.length; i++) {
+          if (res[i].item_id === answer.whichProduct) {
+            chosenItem = res[i];
+          }
+        }
+
+        console.log("stock qty: " + chosenItem.stock_quantity);
+        // if (chosenItem.stock_quantity > parseInt(answer.qtyProduct)) {
+        //   // bid was high enough, so update db, let the user know, and start over
+        //   connection.query(
+        //     "UPDATE products SET ? WHERE ?",
+        //     [
+        //       {
+        //         stock_quantity: chosenItem.stock_quantity - answer.qtyProduct
+        //       },
+        //       {
+        //         item_id: chosenItem.item_id
+        //       }
+        //     ],
+        //     function(cb) {
+        //       console.log(cb);
+
+        //       console.log("Purchase logged successfully!");
+        //       //   start();
+        //     }
+        //   );
+        // } else {
+        //   // bid wasn't high enough, so apologize and start over
+        //   console.log("Not enough in stock...");
+        //   //   start();
+        // }
+      });
   });
 }
